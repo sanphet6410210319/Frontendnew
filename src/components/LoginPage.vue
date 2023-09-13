77e56b3ee0cddcaf31dfd7ec030ade1d92aff97c
sanphet6410210319 <6410210319@psu.ac.th>
2023-09-13 12:02:56 +0700

    lab9

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,27 +1,28 @@
 <template>
-    <v-form ref="LoginForm" :lazy-validation="lazy">
+    <v-form ref="loginForm" :lazy-validation="lazy">
         <v-card width="50%" height="100%">
-        <v-card-title>เข้าสู่ระบบ</v-card-title>
+        <v-card-title style="font-size: 30px !important;">เข้าสู่ระบบ</v-card-title>
         <v-card-text>
         <!-- username -->
         <v-row dense>
             <v-col cols="12" align="start">
                 <span>ชื่อผู้ใช้งาน: </span><br/>
-                <v-text-field v-model="username" rounded outlind dense :rules="Rules.usernameRules"></v-text-field>
+                <v-text-field counter="20" outlined label="ชื่อผู้ใช้งาน" v-model="username" rounded outlind dense :rules="Rules.usernameRules"></v-text-field>
             </v-col>
             </v-row>
             <!-- password -->
             <v-row dense>
                 <v-col cols="12" align="start">
                     <span>รหัสผ่าน:</span><br/>
-                    <v-text-field v-model="password" rounded outline dense :rules="Rules.passwordRules"></v-text-field>
+                    <v-text-field  :disabled="!valid" counter="20" outlined  label="รหัสผ่าน" v-model="password"  rounded outline dense  :type="show1 ? 'text' : 'password'" :rules="Rules.passwordRules"></v-text-field>
                 </v-col>
             </v-row>
         </v-card-text>
         <v-card-actions>
-            <v-btn block rounded color="17B169" class="white--text" @click="login()">เข้าสู่ระบบ</v-btn>
+            <v-btn block :disable="!valid"  color="#4DD0E1" class="white--text" @click="Login()">เข้าสู่ระบบ</v-btn>
         </v-card-actions>
      </v-card>
+     
     </v-form>
 </template>
 <script>
@@ -29,14 +30,19 @@ export default {
     data () {
         return {
             lazy: false,
+            valid: true,
             username: '',
             password: '',
             Rules:{
                 usernameRules:[
-                    v => !!v || 'กรุณากรอกชื่อผู้ใช้งาน'
+                    v => !!v || 'กรุณากรอกชื่อผู้ใช้งาน',
+                    v => (v && v.length <= 20) || 'กรุณากรอกชื่อผู้ใช้งานห้ามเกิน 20 ตัวอักษร'
                 ],
+            
                 passwordRules:[
-                    v =>!!v || 'กรุณากรอกรหัสผ่าน'
+                    v =>!!v || 'กรุณากรอกรหัสผ่าน',
+                    v => (v && v.length <= 20) || 'กรุณากรอกรหัสผ่านห้ามเกิน 20 ตัวอักษร'
+
                 ]
 
             }
@@ -44,12 +50,12 @@ export default {
 
     },
     methods:{
-        login () {
-            if (this.$refs.LoginForm.validate(true)){
+        Login () {
+            if (this.$refs.loginForm.validate(true)){
                 localStorage.setItem('username', this.username)
                 this.$EventBus.$emit('getUsername')
                 this.$EventBus.$emit('checkLoign')
-                this.$route.push({ path: '/' }).catch(() => {})
+                this.$router.push({ path: '/' }).catch(() => {})
             }
 
         }
